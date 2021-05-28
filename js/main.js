170e360ab1d2ef55b6a03bd683253f33434058c7
karimovjamshid <67834450+karimovjamshid@users.noreply.github.com>
2021-05-29 00:56:21 +0500

    Version 5.00

--- js/main.js
+++ js/main.js
@@ -66,12 +66,63 @@ miniNavLink.on('click', function(){
 
 // ======== End Filter ========
 
+    const modal = document.querySelectorAll('.modal');
+    const weightCardCounter = document.querySelectorAll('.weight-card__counter');
+    const modalWindow = document.querySelectorAll('.modal-window');
+    const modalWindowClose = document.querySelectorAll('.modal-window__close');
 
+    const insuranceModal = document.getElementById('insurance-modal');
+    const ratesModal = document.getElementById('rates-modal');
+    const customizedModal = document.getElementById('customized-modal');
+    const customModal = document.getElementById('custom-modal');
+    const bagsModal = document.getElementById('bags-modal');
+
+    const insuranceTitle = document.getElementById('insuranceTitle');
+    const ratesTitle = document.getElementById('ratesTitle');
+    const customizedTitle = document.getElementById('customizedTitle');
+    const customTitle = document.getElementById('customTitle');
     
+    modal.forEach(function (item) {
+        item.addEventListener('click', function () {
+            this.classList.remove('active');
+        })
+    })
+    insuranceTitle.addEventListener('click', function (w) {
+        w.preventDefault();
+        insuranceModal.closest('.modal').classList.add('active');
+    })
+    ratesTitle.addEventListener('click', function (w) {
+        w.preventDefault();
+        ratesModal.closest('.modal').classList.add('active');
+    })
+    customizedTitle.addEventListener('click', function (w) {
+        w.preventDefault();
+        customizedModal.closest('.modal').classList.add('active');
+    })
+    customTitle.addEventListener('click', function (w) {
+        w.preventDefault();
+        customModal.closest('.modal').classList.add('active');
+    })
+    modalWindow.forEach(function (item) {
+        item.addEventListener('click', function (e) {
+         e.stopPropagation();
+        })
+    })
+    modalWindowClose.forEach(function (item) {
+        item.addEventListener('click', function (e) {
+         this.closest('.modal').classList.remove('active');
+        })
+    })
     
-    
+weightCardCounter.forEach(function (item) {
+        item.addEventListener('click', function () {
+            bagsModal.closest('.modal').classList.add('active');
+        })
+    })
     
 
 })
+    
+    
 
 });
